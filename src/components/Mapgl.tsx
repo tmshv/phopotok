f13a1c45fa5d8a2fa0406258d702b389279a5613
Roman Timashev <roman@tmshv.ru>
2025-03-06 02:57:59 +0300

    adds heatmap layer

--- src/components/Mapgl.tsx
+++ src/components/Mapgl.tsx
@@ -47,6 +47,80 @@ export default function Mapgl({ mode }: MapglProps) {
                     "circle-stroke-width": 1,
                 }}
             />
+
+            <Layer
+                id="heatmap"
+                source="neryungry"
+                type="heatmap"
+                layout={{
+                    visibility: vis(mode === "Heatmap"),
+                }}
+                // maxzoom={10}
+                paint={{
+                    // Increase the heatmap weight based on frequency and property magnitude
+                    'heatmap-weight': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'score'],
+                        0,
+                        0,
+                        1,
+                        1
+                    ],
+                    // Increase the heatmap color weight weight by zoom level
+                    // heatmap-intensity is a multiplier on top of heatmap-weight
+                    'heatmap-intensity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        10,
+                        1,
+                        15,
+                        3
+                    ],
+                    // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                    // Begin color ramp at 0-stop with a 0-transparency color
+                    // to create a blur-like effect.
+                    'heatmap-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['heatmap-density'],
+                        0,
+                        'rgba(33,102,172,0)',
+                        0.2,
+                        'rgb(103,169,207)',
+                        0.4,
+                        'rgb(209,229,240)',
+                        0.6,
+                        'rgb(253,219,199)',
+                        0.8,
+                        'rgb(239,138,98)',
+                        1,
+                        'rgb(178,24,43)'
+                    ],
+                    // Adjust the heatmap radius by zoom level
+                    'heatmap-radius': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        10,
+                        1,
+                        15,
+                        20
+                    ],
+                    // Transition from heatmap to circle layer by zoom level
+                    'heatmap-opacity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        9,
+                        0,
+                        10,
+                        1
+                    ]
+                }}
+            />
+
             {mode !== "Photos" ? null : (
                 <PhotoLayer
                     featuresLayerId="invisible-points"
